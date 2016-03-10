656457e625646946508740a154aea9d73b770ca8
Michael Radionov <micrddrgn@gmail.com>
2016-03-11 00:19:04 +0300

    Create reusable snap button with optional displays for desktop and selection

--- src/renderers/user/index.js
+++ src/renderers/user/index.js
@@ -9,35 +9,28 @@ const ipcRenderer = electron.ipcRenderer;
 const screens = require('../../shared/screens');
 
 
-var SnapDesktopDefault = React.createClass({
-  displayName: 'SnapDesktopDefault',
-  handleClick: function () {
-    ipcRenderer.send('snapshot-initiated', {
-      type: 'desktop'
-    });
-  },
-  render: function () {
-    return (
-      React.DOM.button({ onClick: this.handleClick }, 'Default desktop')
-    );
-  }
-});
+function ClickableButton(props, children) {
+  return (
+    React.DOM.button({ onClick: props.onClick }, props.children)
+  );
+}
 
-var SnapDesktopDisplays = React.createClass({
-  displayName: 'SnapDesktopDisplays',
+var DisplayList = React.createClass({
+  displayName: 'DisplayList',
+
   handleChange: function (event) {
     var select = event.target;
     var displayId = Number(select.options[select.selectedIndex].value);
-
-    ipcRenderer.send('snapshot-initiated', {
-      type: 'desktop',
-      displayId: displayId
-    });
+    this.props.onChange(displayId);
   },
-  render: function () {
 
-    var displays = [{ id: '', name: 'Pick display...' }];
-    displays = displays.concat(screens.getNames());
+  render: function () {
+    if (!(this.props.displays.length > 1)) {
+      return false;
+    }
+
+    var placeholder = { id: '', name: 'Pick display...' };
+    var displays = [placeholder].concat(this.props.displays);
 
     var options = displays.map(function (display) {
       return React.DOM.option(
@@ -46,19 +39,37 @@ var SnapDesktopDisplays = React.createClass({
       );
     });
 
+    // TODO: it should not be a select
     return (
       React.DOM.select({ onChange: this.handleChange }, options)
     );
   }
 });
 
-var SnapDesktop = React.createClass({
-  displayName: 'SnapDesktop',
+var SnapDisplaysControl = React.createClass({
+  displayName: 'SnapDisplaysControl',
+  handleButtonClick: function () {
+    this.props.onSnap({
+      type: this.props.type
+    });
+  },
+  handleListChange: function (displayId) {
+    this.props.onSnap({
+      type: this.props.type,
+      displayId: displayId
+    });
+  },
   render: function () {
     return (
       React.DOM.div(null,
-        React.createElement(SnapDesktopDefault),
-        React.createElement(SnapDesktopDisplays)
+        React.createElement(ClickableButton, {
+            onClick: this.handleButtonClick
+          }, this.props.title
+        ),
+        React.createElement(DisplayList, {
+          displays: this.props.displays,
+          onChange: this.handleListChange
+        })
       )
     );
   }
@@ -66,9 +77,31 @@ var SnapDesktop = React.createClass({
 
 var SnapPanel = React.createClass({
   displayName: 'SnapPanel',
+  getInitialState: function () {
+    return { displays: screens.getNames() };
+  },
+  handleSnap: function (options) {
+    ipcRenderer.send('snapshot-initiated', {
+      type: options.type,
+      displayId: options.displayId
+    });
+  },
   render: function () {
     return (
-      React.createElement(SnapDesktop)
+      React.DOM.div(null,
+        React.createElement(SnapDisplaysControl, {
+          type: 'desktop',
+          title: 'Desktop',
+          displays: this.state.displays,
+          onSnap: this.handleSnap
+        }),
+        React.createElement(SnapDisplaysControl, {
+          type: 'selection',
+          title: 'Selection',
+          displays: this.state.displays,
+          onSnap: this.handleSnap
+        })
+      )
     );
   }
 });
