f1ee3e5b8938f3006c48d9be1dde3f7a1b50b123
Michael Radionov <micrddrgn@gmail.com>
2016-05-10 00:38:08 +0300

    Improve container to work with different data structures

--- src/container.js
+++ src/container.js
@@ -1,34 +1,82 @@
 //------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
-
-import _ from 'lodash';
-
-//------------------------------------------------------------------------------
 // Module
 //------------------------------------------------------------------------------
 
 export default class Container {
 
   constructor() {
-    const modules = {};
+    const providers = {};
+
+    const TYPE_VALUE = 'value';
+    const TYPE_FACTORY = 'factory';
+    const TYPE_CLASS = 'class';
 
     return {
 
-      register(nameOrMap, module) {
-        if (_.isPlainObject(nameOrMap)) {
-          _.extend(modules, nameOrMap);
-        } else {
-          modules[nameOrMap] = module;
-        }
-        return this;
+      registerValue(name, value) {
+        providers[name] = {
+          type: TYPE_VALUE,
+          value,
+        };
       },
 
-      get(nameOrNames) {
-        if (_.isArray(nameOrNames)) {
-          return nameOrNames.map(name => modules[name]);
+      registerFactory(name, factory) {
+        providers[name] = {
+          type: TYPE_FACTORY,
+          instance: null,
+          instantiated: false,
+          factory,
+        };
+      },
+
+      registerClass(name, constructor) {
+        providers[name] = {
+          type: TYPE_CLASS,
+          instance: null,
+          instantiated: false,
+          constructor,
+        };
+      },
+
+      registerValues(map) {
+        Object.keys(map).forEach(name => this.registerValue(name, map[name]));
+      },
+
+      registerClasses(map) {
+        Object.keys(map).forEach(name => this.registerClass(name, map[name]));
+      },
+
+      registerFactories(map) {
+        Object.keys(map).forEach(name => this.registerFactory(name, map[name]));
+      },
+
+      get(name) {
+        const provider = providers[name];
+        if (!provider) {
+          return undefined;
         }
-        return modules[nameOrNames];
+
+        if (provider.type === TYPE_VALUE) {
+          return provider.value;
+        }
+
+        if (provider.instantiated) {
+          return provider.instance;
+        }
+
+        if (provider.type === TYPE_FACTORY) {
+          provider.instance = provider.factory();
+        } else if (provider.type === TYPE_CLASS) {
+          provider.instance = new provider.constructor();
+        }
+
+        provider.instantiated = true;
+
+        return provider.instance;
+      },
+
+      pick(names) {
+        return names.map(name => this.get(name));
       },
 
     };
--- test/unit/container.test.js
+++ test/unit/container.test.js
@@ -10,44 +10,126 @@ import Container from '../../src/container';
 // Test
 //------------------------------------------------------------------------------
 
-test('container: register and get single module', (assert) => {
+test('container: register and get value', (assert) => {
   const container = new Container();
-  const module = { foo: 42 };
 
-  container.register('module1', module);
+  container.registerValue('foo', 42);
 
-  assert.equal(container.get('module1'), module);
+  assert.equal(container.get('foo'), 42);
   assert.end();
 });
 
-test('container: register multiple modules', (assert) => {
+test('container: register multiple values', (assert) => {
   const container = new Container();
-  const module1 = { foo: 42 };
-  const module2 = { bar: true };
 
-  container.register({
-    module1,
-    module2,
+  container.registerValues({
+    foo: 42,
+    bar: true,
   });
 
-  assert.equal(container.get('module1'), module1);
-  assert.equal(container.get('module2'), module2);
+  assert.equal(container.get('foo'), 42);
+  assert.equal(container.get('bar'), true);
   assert.end();
 });
 
-test('container: get multiple modules as array', (assert) => {
+test('container: get undefined value', (assert) => {
   const container = new Container();
-  const module1 = { foo: 42 };
-  const module2 = { bar: true };
-  const module3 = { baz: 'qux' };
 
-  container.register({
-    module1,
-    module2,
-    module3,
+  assert.equal(container.get('foo'), undefined);
+  assert.end();
+});
+
+test('container: override value', (assert) => {
+  const container = new Container();
+
+  container.registerValue('foo', 42);
+  container.registerValue('foo', 33);
+
+  assert.equal(container.get('foo'), 33);
+  assert.end();
+});
+
+test('container: override value when registering multiple', (assert) => {
+  const container = new Container();
+
+  container.registerValues({
+    foo: 42,
+    bar: true,
+    foo: 33,
   });
-  const modules = container.get(['module3', 'module1', 'module2']);
 
-  assert.deepEqual(modules, [module3, module1, module2]);
+  assert.equal(container.get('foo'), 33);
+  assert.equal(container.get('bar'), true);
+  assert.end();
+});
+
+test('container: register class and get instance', (assert) => {
+  const container = new Container();
+  const Foo = function Foo() { this.bar = 42; };
+
+  container.registerClass('foo', Foo);
+  const foo = container.get('foo');
+
+  assert.ok(foo instanceof Foo);
+  assert.equal(foo.bar, 42);
+  assert.end();
+});
+
+test('container: register multiple classes', (assert) => {
+  const container = new Container();
+  const Foo = function Foo() { this.bar = 42; };
+  const Baz = function Baz() { this.qux = 33; };
+
+  container.registerClasses({
+    foo: Foo,
+    baz: Baz,
+  });
+  const foo = container.get('foo');
+  const baz = container.get('baz');
+
+  assert.ok(foo instanceof Foo);
+  assert.equal(foo.bar, 42);
+  assert.ok(baz instanceof Baz);
+  assert.equal(baz.qux, 33);
+  assert.end();
+});
+
+test('container: register factory and get instance', (assert) => {
+  const container = new Container();
+  const foo = function foo() { return 42; };
+
+  container.registerFactory('foo', foo);
+
+  assert.equal(container.get('foo'), 42);
+  assert.end();
+});
+
+test('container: register multiple factories', (assert) => {
+  const container = new Container();
+  const foo = function foo() { return 42; };
+  const bar = function bar() { return 33; };
+
+  container.registerFactories({
+    foo,
+    bar,
+  });
+
+  assert.equal(container.get('foo'), 42);
+  assert.equal(container.get('bar'), 33);
+  assert.end();
+});
+
+test('container: get multiple values', (assert) => {
+  const container = new Container();
+  const Baz = function Baz() { this.baz = 42; };
+  const bar = function bar() { return 22; };
+
+  container.registerValue('foo', 33);
+  container.registerFactory('bar', bar);
+  container.registerClass('baz', Baz);
+  container.registerValue('qux', 11);
+  const values = container.pick(['bar', 'baz', 'foo']);
+
+  assert.deepEqual(values, [22, new Baz(), 33]);
   assert.end();
 });
