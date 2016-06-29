27efa93d9ec214b5173d5d64aa396d2c8effbf97
Michael Radionov <micrddrgn@gmail.com>
2016-06-29 21:56:45 +0300

    Add simple version of packager

---
+++ package.js
@@ -0,0 +1,25 @@
+const packager = require('electron-packager');
+
+const pkg = require('./package.json');
+
+const devDeps = Object.keys(pkg.devDependencies);
+
+const ignoreDirs = [
+  '^/test($|/)',
+  '^/tmp($|/)',
+];
+
+const ignoreDeps = devDeps.map(name => `/node_modules/${name}($|/)`);
+
+const options = {
+  arch: 'x64',
+  dir: __dirname,
+  platform: 'linux',
+  ignore: ignoreDirs.concat(ignoreDeps),
+};
+
+packager(options, (err, appPaths) => {
+  if (err) throw err;
+
+  console.log(appPaths);
+});
