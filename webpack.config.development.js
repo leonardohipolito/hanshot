ffa088ee21be2252559add998fd4d6081e55e806
Michael Radionov <micrddrgn@gmail.com>
2016-07-04 18:21:51 +0300

    Split webpack configs for renderer and main processes

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,12 +1,11 @@
 const path = require('path');
 
+const nodeExternals = require('webpack-node-externals');
+
+
 module.exports = {
 
   entry: {
-
-    main: [
-      './src/main.js'
-    ],
 
     'dashboard-window': [
       './src/dashboard-window/renderer/dashboard.jsx',
@@ -35,10 +34,10 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
-      {
-        test: /\.json$/,
-        loader: 'json-loader',
-      },
+      // {
+      //   test: /\.json$/,
+      //   loader: 'json-loader',
+      // },
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader',
@@ -53,25 +52,21 @@ module.exports = {
       // in /node_modules/es6-promise/dist
       // @ ./~/es6-promise/dist/es6-promise.js 131:20-30
 
-      /node_modules\/es6-promise\/dist\/es6-promise\.js/,
+      // /node_modules\/es6-promise\/dist\/es6-promise\.js/,
 
       // https://github.com/webpack/webpack/issues/138
-      /node_modules\/json-schema\/lib\/validate\.js/,
+      // /node_modules\/json-schema\/lib\/validate\.js/,
     ],
   },
 
-  // TODO: find more about
-  //  Compile for usage in Electron -
-  //  supports require-ing Electron-specific modules.
+  // Supports require-ing Electron-specific and node modules
   target: 'electron',
 
-  // TODO: find more about
-  node: {
-    // Depending on the config option node.__dirname:
-    //  mock: equal “/“
-    //  false: Not defined - points to built script directory ???
-    //  true: node.js __dirname - relative path from start directory ???
-    __dirname: false,
-  },
+  externals: [
+    // Do not include node_modules in a bundle
+    nodeExternals({
+      whitelist: ['webpack/hot/dev-server'],
+    }),
+  ],
 
 };
---
+++ webpack.config.electron.js
@@ -0,0 +1,64 @@
+const path = require('path');
+
+const nodeExternals = require('webpack-node-externals');
+
+
+module.exports = {
+
+  entry: {
+
+    main: [
+      './src/main.js'
+    ],
+
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.dist.js',
+  },
+
+  module: {
+
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+    ],
+
+    noParse: [
+
+      // WARNING in ./~/es6-promise/dist/es6-promise.js
+      // Module not found: Error: Cannot resolve module 'vertx'
+      // in /node_modules/es6-promise/dist
+      // @ ./~/es6-promise/dist/es6-promise.js 131:20-30
+
+      /node_modules\/es6-promise\/dist\/es6-promise\.js/,
+    ],
+
+  },
+
+  // Supports require-ing Electron-specific and node modules
+  target: 'electron',
+
+  // TODO: find more about
+  // node: {
+  //   // Depending on the config option node.__dirname:
+  //   //  mock: equal “/“
+  //   //  false: Not defined - points to built script directory ???
+  //   //  true: node.js __dirname - relative path from start directory ???
+  //   __dirname: false,
+  // },
+
+  externals: [
+    // Do not include node_modules in a bundle
+    // nodeExternals()
+  ],
+
+};
